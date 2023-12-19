09a3e38532848af31abd525a24e3e0dcd9034fd0
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2023-12-19 16:07:06 +0800

    Initial commit

---
+++ src/BinarySearch.java
@@ -0,0 +1,34 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int key = 5;
+
+        int result = binarySearch(sortedArray, key);
+
+        if (result != -1) {
+            System.out.println("Element found at index " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+
+    // Binary Search algorithm
+    public static int binarySearch(int[] arr, int key) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == key) {
+                return mid; // Key found
+            } else if (arr[mid] < key) {
+                low = mid + 1; // Search in the right half
+            } else {
+                high = mid - 1; // Search in the left half
+            }
+        }
+
+        return -1; // Key not found
+    }
+}
